13e709e431e5e303b8f38d70842efd59116790a4
psheninada <psheninada@elocont.ru>
2021-03-13 21:17:29 +0300

    переворот чисел

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    let newN = String(n);
+    let numb = newN.split("").reverse();
+    if(numb[0]=='0')
+    {
+        numb.splice(0, 1);
+    }
+    if((numb[numb.length -1]=='-')){
+        numb.splice(numb.length-1, 1);
+    }
+    return numb.join("");
+
 }
